284b490ddc29f58764c74429620e210949d3958f
yamagao <yamagao@126.com>
2015-08-07 15:37:23 -0400

    4h markers

---
+++ data_markers_4h.js
@@ -0,0 +1,12 @@
+/** 4-h Info and Coords
+*** Author: Yiqi Gao
+*** Last Modified: August 7, 2015
+**/
+var fourH = {
+	"fourH": [
+		{"name": "Camp Cherry Lake", "contact": "Neva Baltzell", "description": "Camp Cherry Lake is a place to grow, find peace and<br>make friends. Located on scenic Cherry Lake, rustic<br>cabins, an open-air pavilion and a dining hall are<br>nestled among live oaks on a gently sloping bank.", "address": "3861 NE Cherry Lake Circle, Madison, Florida 32340", "phone": "(850) 929-4429", "site": "http://florida4h.org/camps_/cherrylake/", "email": "cherrylake4-h@ufl.edu", "latitude": 30.620862, "longitude": -83.421815},
+		{"name": "Camp Cloverleaf", "contact": "Steve Cooper", "description": "Camp Cloverleaf is a traditional camp with modern conveniences,<br>combining a relaxed camp atmosphere with today’s camping desires.<br>Nestled on the banks of Lake Francis in Highlands County, its private<br>facilities, open spaces and good fishing allow participants to play,<br>learn, and work in a safe and enjoyable environment.", "address": "126 Cloverleaf Rd. Lake Placid, FL 33852", "phone": "(863) 465-4884", "site": "http://florida4h.org/camps_/cloverleaf/", "email": "cloverleaf4-h@ufl.edu", "latitude": 27.332076, "longitude": -81.401981},
+		{"name": "Camp Ocala", "contact": "Sarah Whitfield", "description": "Camp Ocala is a traditional camp and conference center,<br>located on the shores of Sellers Lake in the Ocala National<br>Forest. Camp Ocala is conveniently close to Juniper and<br>Alexander Springs, and less than an hour from Daytona<br>Beach and Ocala. Modern sleeping, dining and meeting<br>facilities dot the picturesque landscape.", "address": "18533 NFS 535, Altoona, Florida 32702", "phone": "(352) 759-2288", "site": "http://florida4h.org/camps_/camp-ocala/", "email": "ocala4-h@ufl.edu", "latitude": 29.106486, "longitude": -81.625268},
+		{"name": "Camp Timpoochee", "contact": "Jennifer Williams", "description": "Camp Timpoochee is a picturesque camping complex<br>nestled on the shore of the Choctawhatchee Bay.<br>Comfortable sleeping, dining and meeting facilities<br>allow visitors to learn, work and plan in an enjoyable,<br>relaxed environment.", "address": "4750 Timpoochee Lane, Niceville, Florida 32578", "phone": "(850) 897-2224", "site": "http://florida4h.org/camps_/timpoochee/", "email": "timpoochee4-h@ufl.edu", "latitude": 30.454714, "longitude": -86.396928},
+	]
+}
